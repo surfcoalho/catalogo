3ede92c1060960cebc3712e6e605f942d3fcdbd0
adriano.augusto <adriano.augusto@cgti.sde.fortaleza.ce.gov.br>
2017-10-16 09:45:28 -0200

    entidades enums repositories
    Signed-off-by: adriano.augusto <adriano.augusto@cgti.sde.fortaleza.ce.gov.br>

---
+++ src/main/java/com/catalogo/api/repositories/EmpresaRepository.java
@@ -0,0 +1,15 @@
+package com.catalogo.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.catalogo.api.entities.Empresa;
+
+
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
---
+++ src/main/java/com/catalogo/api/repositories/FuncionarioRepository.java
@@ -0,0 +1,18 @@
+package com.catalogo.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.catalogo.api.entities.Funcionario;
+
+
+
+@Transactional(readOnly = true)
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+	Funcionario findByCpf(String cpf);
+	
+	Funcionario findByEmail(String email);
+	
+	Funcionario findByCpfOrEmail(String cpf, String email);
+}
