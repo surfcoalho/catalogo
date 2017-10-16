56b4cd674f954dabb792a0957a133f9e111954e9
adriano.augusto <adriano.augusto@cgti.sde.fortaleza.ce.gov.br>
2017-10-16 13:51:10 -0300

    cadastroPF

--- src/main/java/com/catalogo/api/services/impl/FuncionarioServiceImpl.java
+++ src/main/java/com/catalogo/api/services/impl/FuncionarioServiceImpl.java
@@ -20,21 +20,25 @@ public class FuncionarioServiceImpl implements FuncionarioService {
 	@Autowired
 	private FuncionarioRepository funcionarioRepository;
 	
+	@Override
 	public Funcionario persistir(Funcionario funcionario) {
 		log.info("Persistindo funcionário: {}", funcionario);
 		return this.funcionarioRepository.save(funcionario);
 	}
 	
+	@Override
 	public Optional<Funcionario> buscarPorCpf(String cpf) {
 		log.info("Buscando funcionário pelo CPF {}", cpf);
 		return Optional.ofNullable(this.funcionarioRepository.findByCpf(cpf));
 	}
 	
+	@Override
 	public Optional<Funcionario> buscarPorEmail(String email) {
 		log.info("Buscando funcionário pelo email {}", email);
 		return Optional.ofNullable(this.funcionarioRepository.findByEmail(email));
 	}
 	
+	@Override
 	public Optional<Funcionario> buscarPorId(Long id) {
 		log.info("Buscando funcionário pelo IDl {}", id);
 		return Optional.ofNullable(this.funcionarioRepository.findOne(id));
--- src/main/java/com/catalogo/api/services/impl/LancamentoServiceImpl.java
+++ src/main/java/com/catalogo/api/services/impl/LancamentoServiceImpl.java
@@ -24,23 +24,27 @@ public class LancamentoServiceImpl implements LancamentoService {
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
 
+	@Override
 	public Page<Lancamento> buscarPorFuncionarioId(Long funcionarioId, PageRequest pageRequest) {
 		log.info("Buscando lançamentos para o funcionário ID {}", funcionarioId);
 		return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
 	}
 	
+	@Override
 	@Cacheable("lancamentoPorId")
 	public Optional<Lancamento> buscarPorId(Long id) {
 		log.info("Buscando um lançamento pelo ID {}", id);
 		return Optional.ofNullable(this.lancamentoRepository.findOne(id));
 	}
 	
+	@Override
 	@CachePut("lancamentoPorId")
 	public Lancamento persistir(Lancamento lancamento) {
 		log.info("Persistindo o lançamento: {}", lancamento);
 		return this.lancamentoRepository.save(lancamento);
 	}
 	
+	@Override
 	public void remover(Long id) {
 		log.info("Removendo o lançamento ID {}", id);
 		this.lancamentoRepository.delete(id);
